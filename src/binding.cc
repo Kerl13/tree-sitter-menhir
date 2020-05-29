d969da2c1714675a844bd4ec52854083678c8a4a
Martin Pépin <kerl@wkerl.me>
2020-05-29 15:54:03 +0200

    Bump nan and tree-sitter-cli

--- src/binding.cc
+++ src/binding.cc
@@ -15,12 +15,12 @@ void Init(Local<Object> exports, Local<Object> module) {
   tpl->SetClassName(Nan::New("Language").ToLocalChecked());
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-  Local<Function> constructor = tpl->GetFunction();
+  Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
   Nan::SetInternalFieldPointer(instance, 0, tree_sitter_menhir());
 
-  instance->Set(Nan::New("name").ToLocalChecked(), Nan::New("menhir").ToLocalChecked());
-  module->Set(Nan::New("exports").ToLocalChecked(), instance);
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("menhir").ToLocalChecked());
+  Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
 NODE_MODULE(tree_sitter_menhir_binding, Init)
