69374e23d18b4ac076c9545a66e7094ee615eb3e
Martin Pépin <kerl@wkerl.me>
2020-05-29 16:00:50 +0200

    Update test script wrt the new path of remote tests

--- script/run_menhir_test_suite.sh
+++ script/run_menhir_test_suite.sh
@@ -12,9 +12,9 @@ if [ ! -d "$TEST_DIR/menhir" ]; then
   git clone https://gitlab.inria.fr/fpottier/menhir "$TEST_DIR/menhir"
 fi
 
-rm -f "$TEST_DIR/menhir/test/good/zipperposition.1.5-Parse_zf.mly"
-rm -f "$TEST_DIR/menhir/test/good/boomerang.1.1.0-bparser.mly"
-rm -f "$TEST_DIR/menhir/test/good/ldap.2.4.0-ldap_filterparser.mly"
-rm -f "$TEST_DIR/menhir/test/good/dns.1.0.1-zone_parser.mly"
+rm -f "$TEST_DIR/menhir/test/static/good/zipperposition.1.5-Parse_zf.mly"
+rm -f "$TEST_DIR/menhir/test/static/good/boomerang.1.1.0-bparser.mly"
+rm -f "$TEST_DIR/menhir/test/static/good/ldap.2.4.0-ldap_filterparser.mly"
+rm -f "$TEST_DIR/menhir/test/static/good/dns.1.0.1-zone_parser.mly"
 
-find "$TEST_DIR/menhir/test/good" -name \*.mly -exec "$TS" parse -q '{}' +
+find "$TEST_DIR/menhir/test/static/good" -name \*.mly -exec "$TS" parse -q '{}' +
